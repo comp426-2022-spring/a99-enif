6ff35c8cec2b072d6c1449441f7687dcf74adc81
Shiying Cai <cai2433717514@gmail.com>
2022-04-29 22:57:06 -0400

    statewide api calling update

--- public/statewideScript.js
+++ public/statewideScript.js
@@ -1,7 +1,7 @@
 //var state_code = document.getElementById('stateSelect').value;
 //let state_code_value = state_code.toLowerCase;
 //const api_url = "https://api.covidtracking.com/v2/states/" + state_code + ".json";
-const api_url = "https://api.covidtracking.com/v2/states/mi.json";
+const api_url = "https://api.covidtracking.com/v1/states/ca/current.json";
 async function getAPI(url) {
     const response = await fetch(url);
     var data = await response.json();
@@ -22,18 +22,20 @@ function hideloader() {
 function show(data) {
     let tab = 
         `<tr>
+          <th>Date Checked</th>  
           <th>State</th>
           <th>Positive</th>
           <th>Death</th>
-          <th>Hospitalized Currently<th>
+          <th>Hospitalized Currently</th>
          </tr>`;  
 
 
         tab += `<tr> 
-            <td>${data.name} </td>
+            <td>${data.dateChecked}</td>
+            <td>${data.state} </td>
             <td>${data.positive}</td>
-            <td>${data.outcomes.death.confirmed}</td> 
-            <td>${data.hospitalization.hospitalized.currently}</td>          
+            <td>${data.death}</td> 
+            <td>${data.hospitalizedCurrently}</td>          
         </tr>`;
     
 
